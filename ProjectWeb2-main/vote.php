417c18b1b4a902324f544c64ccb003880a05ec00
Dương Minh Trí <3122410420@sv.sgu.edu.vn>
2025-03-14 23:58:25 +0700

    Fix phân trang sản phẩm theo danh mục & lỗi đánh giá/feedback

--- ProjectWeb2-main/vote.php
+++ ProjectWeb2-main/vote.php
@@ -1,13 +1,24 @@
 <?php 
 include("./includes/header.php");
-if (!isset($_SESSION['auth_user']['id'])){
-    die("Từ Chối truy cập <a href='./login'>Đăng nhập ngay</a> ");
+include_once("./functions/userfunctions.php");
+
+
+if (!isset($_SESSION['auth_user']['id'])) {
+    die("Từ chối truy cập <a href='./login'>Đăng nhập ngay</a> ");
 }
-$id = $_GET['id'];
-?>;
+
+// Lấy ID sản phẩm và ép kiểu để tránh lỗi SQL Injection
+$id = isset($_GET['id']) ? intval($_GET['id']) : 0;
+
+if ($id <= 0) {
+    die("Không tìm thấy sản phẩm! <a href='./cart-status.php'>Trở lại</a>");
+}
+
+// Lấy thông tin sản phẩm
+$product = getMyOrderVote($id);
+?>
 <link rel="stylesheet" href="./assets/css/vote.css">
 <body>
-    <!-- product-detail content -->
     <div class="bg-main">
         <div class="container">
             <div class="box">
@@ -24,69 +35,59 @@ $id = $_GET['id'];
 
             <div class="box" style="padding: 0 40px">
                 <div class="product-info">
-                <?php
-                    $product = getMyOrderVote($id);
-                    if (mysqli_num_rows($product) == 0){       
+                <?php if (!$product) { 
+                    http_response_code(404); // Trả về mã lỗi 404 nếu không tìm thấy
                 ?>
                     <p style="font-size: 20px; text-align: center;">
-                      Mặt hàng này chưa thể vote <a style="color: blue; text-decoration: underline" href="./cart-status.php">Trở lại</a>  
+                        Mặt hàng này chưa thể vote <a style="color: blue; text-decoration: underline" href="./cart-status.php">Trở lại</a>  
                     </p>
-                <?php 
-                    } else { 
-                        $product = mysqli_fetch_array($product);
-                ?>
+                <?php } else { ?>
                     <div class="box" style="padding: 0 100px">
                         <div class="product-info">
-                            <h1>
-                                <?= $product['name'] ?>
-                            </h1>
+                            <h1><?= htmlspecialchars($product['name']) ?></h1>
                             <p class="product-description">
-                                <?= nl2br($product['small_description']) ?>
+                                <?= nl2br(htmlspecialchars($product['small_description'])) ?>
                             </p>
                         </div>
                         <br>
                         <div class="product-img" id="product-img">
-                            <img src="./images/<?= $product['image'] ?>" alt="">
+                            <img src="./images/<?= htmlspecialchars($product['image']) ?>" alt="Hình ảnh sản phẩm">
                         </div>
                         <br>
                         <div class="product-detail-description">
-                            <?= $product['description'] ?>
+                            <?= nl2br(htmlspecialchars($product['description'])) ?>
                         </div>
                     </div>
-                <?php } 
-                ?>
 
-
-                <form class="form-vote" method="post" action="./functions/ordercode.php">
-                    <div class="rate-form">
-                        <input type="hidden" name="rate" value="true">
-                        <input type="hidden" name="id" value="<?= $id ?>">
-                        <fieldset class="rating">
-                            <input type="radio" id="star5" name="rating" <?php if ($product['rate'] == 5) echo "checked"?> value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
-                            <input type="radio" id="star4" name="rating" <?php if ($product['rate'] == 4) echo "checked"?> value="4" /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
-                            <input type="radio" id="star3" name="rating" <?php if ($product['rate'] == 3) echo "checked"?> value="3" /><label class = "full" for="star3" title="Meh - 3 stars"></label>
-                            <input type="radio" id="star2" name="rating" <?php if ($product['rate'] == 2) echo "checked"?> value="2" /><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
-                            <input type="radio" id="star1" name="rating" <?php if ($product['rate'] == 1) echo "checked"?> value="1" /><label class = "full" for="star1" title="Sucks big time - 1 star"></label>
-                        </fieldset>
-                    </div>
-                    <br>
-                    <div class="comment">
-                        <textarea name="comment"><?= $product['comment'] ?></textarea>
-                    </div>
-                    <br>
-                    <div class="comment">
-                        <button class="btn-vote">
-                            Đánh giá
-                        </button>
-                    </div>
-                </form>
-                <br>
-                <br>
+                    <form class="form-vote" method="post" action="./functions/ordercode.php">
+                        <div class="rate-form">
+                            <input type="hidden" name="rate" value="true">
+                            <input type="hidden" name="id" value="<?= $id ?>">
+                            <fieldset class="rating">
+                                <?php 
+                                $currentRate = $product['rate'] ?? 0;
+                                for ($i = 5; $i >= 1; $i--) {
+                                    $checked = ($currentRate == $i) ? 'checked' : '';
+                                    echo "<input type='radio' id='star$i' name='rating' value='$i' $checked />";
+                                    echo "<label class='full' for='star$i' title='Đánh giá $i sao'></label>";
+                                }
+                                ?>
+                            </fieldset>
+                        </div>
+                        <br>
+                        <div class="comment">
+                            <textarea name="comment"><?= htmlspecialchars($product['comment'] ?? '') ?></textarea>
+                        </div>
+                        <br>
+                        <div class="comment">
+                            <button class="btn-vote">Đánh giá</button>
+                        </div>
+                    </form>
+                <?php } ?>
                 </div>
             </div>
         </div>
     </div>
-    <!-- end product-detail content -->
     <?php include("./includes/footer.php") ?>
     <script src="./assets/js/app.js"></script>
     <script src="./assets/js/index.js"></script>
