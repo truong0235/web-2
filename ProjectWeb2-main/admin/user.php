ad5319939644b86c69ef47bbfe634e08a03995d9
poro <quangkietle382@gmail.com>
2025-03-15 13:41:32 +0700

    xong phan them xoa sua nguoidung

--- ProjectWeb2-main/admin/user.php
+++ ProjectWeb2-main/admin/user.php
@@ -16,6 +16,11 @@ $users = getAllUsers();
                             <h6 class="text-white text-capitalize ps-3">Người dùng</h6>
                         </div>
                     </div>
+                    <div class="card-header">
+                        <form action="./add_user.php" method="POST">
+                            <button type="submit" name="delete_product_btn" class="btn btn-danger" style="background-color: green;">Thêm </button>
+                        </form>
+                    </div>
                     <div class="card-body px-0 pb-2">
                         <div class="table-responsive p-0">
                             <table class="table align-items-center mb-0">
@@ -26,10 +31,12 @@ $users = getAllUsers();
                                         <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-10 ps-2">Địa chỉ</th>
                                         <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-10">Tổng đơn</th>
                                         <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-10">Ngày bắt đầu</th>
+                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-10">Tùy chỉnh</th>
+                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-10">Khóa</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <?php foreach($users as $user) { ?>
+                                    <?php foreach ($users as $user) { ?>
                                         <tr>
                                             <td>
                                                 <div class="d-flex px-2 py-1">
@@ -41,21 +48,38 @@ $users = getAllUsers();
                                             </td>
                                             <td>
                                                 <p class="text-xs font-weight-bold mb-0">
-                                                <?= $user['phone']?>
+                                                    <?= $user['phone'] ?>
                                                 </p>
                                             </td>
                                             <td>
                                                 <p class="text-xs font-weight-bold mb-0">
-                                                    <?= $user['address']?>
+                                                    <?= $user['address'] ?>
                                                 </p>
                                             </td>
                                             <td class="align-middle text-center text-sm">
-                                                <?= $user['total_buy']?>
+                                                <?= $user['total_buy'] ?>
                                             </td>
                                             <td class="align-middle text-center">
                                                 <span class="text-secondary text-xs font-weight-bold">
                                                     <?= date('d-m-Y', strtotime($user['creat_at'])); ?>
                                                 </span>
+                                            </td>
+                                            <td class="align-middle text-center">
+                                                <form action="../functions/authcode.php" method="POST" onsubmit="return confirm('Bạn có chắc chắn muốn xóa người dùng này không?');">
+                                                    <input type="hidden" name="user_id" value="<?= $user['id'] ?>" />
+                                                    <button type="submit" name="delete_user_btn" class="btn btn-danger">Xóa </button>
+                                                </form>
+                                                <form action="./edit_user.php" method="POST">
+                                                    <input type="hidden" name="user_id" value="<?= $user['id'] ?>" />
+                                                    <button name="update_user_btn" class="btn btn-danger" style="background-color: blue; ">Sửa </button>
+                                                </form>
+                                            </td>
+                                            <td class="align-middle text-center">
+                                                <form action="../functions/authcode.php" method="POST">
+                                                    <input type="hidden" name="user_id" value="<?= $user['id'] ?>" />
+                                                    <input type="checkbox" name="lock_user" style="transform: scale(2.5);" onchange="this.form.submit()"
+                                                        <?= $user['role_as'] == 2 ? 'checked' : '' ?>> </input>
+                                                </form>
                                             </td>
                                         </tr>
                                     <?php } ?>
